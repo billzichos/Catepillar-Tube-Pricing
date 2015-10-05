228c96fd61ba45560db0606f1ce3decd327c5d39
billzichos <billzichos@hotmail.com>
2015-10-04 20:10:13 -0400

    gather data

--- gather_data.R
+++ gather_data.R
@@ -7,19 +7,6 @@ setwd(wd)
 #   - data.zip
 
 source("~/GitHub/Get-Raw-Data/download.R")
+
 downloadKaggle("caterpillar-tube-pricing","sample_submission.csv")
-downloadKaggle("caterpillar-tube-pricing","data.zip")
-
-
-trnFile <- "train.csv"
-tstFile <- "test.csv"
-
-download.file(trnUrl, paste(wd,"\\train.csv", sep=""), quiet=TRUE)
-download.file(tstUrl, paste(wd,"\\test.csv", sep=""), quiet=TRUE)
-
-datalist <- list(
-        train=read.csv(
-                paste(wd,"\\",trnFile, sep=""), header=TRUE, as.is=TRUE), 
-        test=read.csv(
-                paste(wd,"\\",tstFile, sep=""), header=TRUE, as.is=TRUE)
-        )
+downloadMultKaggleZip("caterpillar-tube-pricing","data.zip", "competition_data")
